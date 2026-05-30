8efbe5d643bfbf9313ab1eccc44b4735da37184f
martand-codes <prakharmartand755@gmail.com>
2026-05-30 12:19:21 +0530

    chore(frontend): establish frontend architecture

---
+++ Frontend/src/Pages/Landing/Products/LeftComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftComp = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LeftComp
---
+++ Frontend/src/Pages/Landing/Products/RightComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightComp = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RightComp
