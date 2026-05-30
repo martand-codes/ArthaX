5cf386cc384dda6b54b8d4862aab3d21a79275cc
Martand-Prakhar <prakharmartand755@gmail.com>
2026-05-30 11:15:00 +0530

    Frontend Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-amber-700'>Hii This the Frontend Setup</h1>
+    </div>
+  )
+}
+
+export default App
