189aca650afdb4f2c16865400f9d05708bba1d34
martand-codes <prakharmartand755@gmail.com>
2026-07-25 14:34:22 +0530

    build(backend): initialized backend folder structure, installed dependencies and containarized the postgres database

---
+++ Backend/src/server.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import app from './app';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+  console.log(` Server is listening on port ${PORT}`);
+});
